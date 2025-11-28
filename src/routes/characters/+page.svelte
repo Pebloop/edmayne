5d8cdd130d18ca13b78271449bec7759c8527a8d
Pebloop <pebloop@outlook.com>
2025-11-28 09:40:03 +0100

    Fix characters page

--- src/routes/characters/+page.svelte
+++ src/routes/characters/+page.svelte
@@ -4,10 +4,10 @@
 <div class=" container mx-auto p-4">
 	<h1 class="text-4xl font-bold">Characters of Edmayne</h1>
 
-	<ul class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+	<ul class="grid grid-cols-1 gap-6">
 			<li class="bg-white rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300">
 				<a href="/characters/tempo" class="flex flex-row">
-					<img src="/characters/tempo/icon.png" alt="Tempo Light" class="img object-cover mb-4 rounded" />
+					<img src="/characters/tempo/icon.png" alt="Tempo Light" class="img object-cover rounded aspect-square" />
 					<div class="p-6">
 						<h2 class="text-2xl font-bold mb-2">Tempo Light</h2>
 						<p class="text-gray-700 mb-4">The last Time dragon there is. Adopted by the Light family.</p>
@@ -17,7 +17,7 @@
 
 			<li class="bg-white rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300">
 				<a href="/characters/venom" class="flex flex-row">
-					<img src="/characters/venom/icon.png" alt="Venom" class="img object-cover mb-4 rounded" />
+					<img src="/characters/venom/icon.png" alt="Venom" class="img object-cover rounded aspect-square" />
 					<div class="p-6">
 						<h2 class="text-2xl font-bold mb-2">Venom</h2>
 						<p class="text-gray-700 mb-4">The last War dragon there is. Living and fighting for the rebellion.</p>
@@ -27,7 +27,7 @@
 
 			<li class="bg-white rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300">
 				<a href="/characters/thunder" class="flex flex-row">
-					<img src="/characters/thunder/icon.png" alt="Thunder Lightning" class="img object-cover mb-4 rounded" />
+					<img src="/characters/thunder/icon.png" alt="Thunder Lightning" class="img object-cover rounded" />
 					<div class="p-6">
 						<h2 class="text-2xl font-bold mb-2">Thunder Lightning</h2>
 						<p class="text-gray-700 mb-4">The prince of the desert country.</p>
@@ -37,7 +37,7 @@
 
 		<li class="bg-white rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300">
 			<a href="/characters/ocean" class="flex flex-row">
-				<img src="/characters/ocean/icon.png" alt="Ocean Water" class="img object-cover mb-4 rounded" />
+				<img src="/characters/ocean/icon.png" alt="Ocean Water" class="img object-cover rounded" />
 				<div class="p-6">
 					<h2 class="text-2xl font-bold mb-2">Ocean Water</h2>
 					<p class="text-gray-700 mb-4">The prince of the underwater country.</p>
@@ -47,7 +47,7 @@
 
 		<li class="bg-white rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300">
 			<a href="/characters/dusk" class="flex flex-row">
-				<img src="/characters/dusk/icon.jpg" alt="Dusk Underground" class="img object-cover mb-4 rounded" />
+				<img src="/characters/dusk/icon.jpg" alt="Dusk Underground" class="img object-cover rounded" />
 				<div class="p-6">
 					<h2 class="text-2xl font-bold mb-2">Dusk Underground</h2>
 					<p class="text-gray-700 mb-4">The prince of the underground country.</p>
@@ -57,7 +57,7 @@
 
 		<li class="bg-white rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300">
 			<a href="/characters/sky" class="flex flex-row">
-				<img src="/characters/sky/icon.png" alt="Sky Cloud" class="img object-cover mb-4 rounded" />
+				<img src="/characters/sky/icon.png" alt="Sky Cloud" class="img object-cover rounded" />
 				<div class="p-6">
 					<h2 class="text-2xl font-bold mb-2">Sky Cloud</h2>
 					<p class="text-gray-700 mb-4">The prince of the sky country.</p>
@@ -67,7 +67,7 @@
 
 		<li class="bg-white rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300">
 			<a href="/characters/blaze" class="flex flex-row">
-				<img src="/characters/blaze/icon.png" alt="Blaze Volcano" class="img object-cover mb-4 rounded" />
+				<img src="/characters/blaze/icon.png" alt="Blaze Volcano" class="img object-cover rounded" />
 				<div class="p-6">
 					<h2 class="text-2xl font-bold mb-2">Blaze Volcano</h2>
 					<p class="text-gray-700 mb-4">The prince of the lava country.</p>
@@ -77,7 +77,7 @@
 
 		<li class="bg-white rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300">
 			<a href="/characters/peak" class="flex flex-row">
-				<img src="/characters/peak/icon.jpg" alt="Peak Mountain" class="img object-cover mb-4 rounded" />
+				<img src="/characters/peak/icon.jpg" alt="Peak Mountain" class="img object-cover rounded" />
 				<div class="p-6">
 					<h2 class="text-2xl font-bold mb-2">Peak mountain</h2>
 					<p class="text-gray-700 mb-4">The prince of the mountain country.</p>
@@ -87,7 +87,7 @@
 
 		<li class="bg-white rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300">
 			<a href="/characters/kuro" class="flex flex-row">
-				<img src="/characters/kuro/icon.png" alt="Kuro Light" class="img object-cover mb-4 rounded" />
+				<img src="/characters/kuro/icon.png" alt="Kuro Light" class="img object-cover rounded" />
 				<div class="p-6">
 					<h2 class="text-2xl font-bold mb-2">Kuro Light</h2>
 					<p class="text-gray-700 mb-4">The personal servant of Tempo.</p>
@@ -97,7 +97,7 @@
 
 		<li class="bg-white rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300">
 			<a href="/characters/shiro" class="flex flex-row">
-				<img src="/characters/shiro/icon.png" alt="Shiro" class="img object-cover mb-4 rounded" />
+				<img src="/characters/shiro/icon.png" alt="Shiro" class="img object-cover rounded" />
 				<div class="p-6">
 					<h2 class="text-2xl font-bold mb-2">Shiro</h2>
 					<p class="text-gray-700 mb-4">The chief of the rebellion.</p>

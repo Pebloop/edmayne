eafe6dd5d178590a5aa7754fbe18ef540ffcc010
pebloop <pebloop@outlook.com>
2025-12-28 17:54:12 +0100

    Add continent

---
+++ src/routes/continent/+page.svelte
@@ -0,0 +1,87 @@
+<script>
+</script>
+
+<div class=" container mx-auto p-4 page">
+	<h1 class="text-4xl font-bold mb-4">The continent of edmayne</h1>
+
+	<div class="flex flex-col items-center">
+		<div class="w-full md:w-1/2 p-2 bg-white rounded-lg shadow-md">
+
+			<img src="/continent/edmayne_map.png" alt="Map of edmayne" class="w-9/10"/>
+
+			<h2 class="text-2xl font-bold mb-2">About</h2>
+
+			<p>
+				Edmayne is an isolated continent surrounded by the barriers,
+				a big region of water filled with sea monsters and dangerous currents
+				that prevent anyone from entering or leaving the continent.
+			</p>
+
+			<p>
+				The continent is divided into 3 parts, The good lands, the corrupted lands and the isolated lands.
+				Those regions don't communicate much with each others, the corrupted lands being too hostile to travel through
+				and being believed to be cursed by the good lands inhabitants.
+			</p>
+
+			<div class="flex justify-center">
+				<img src="/continent/edmayne_regions_map.png" alt="region maps" class="mt-4"/>
+			</div>
+
+			<h2>The good lands</h2>
+
+			<p>
+				The good lands are made of 7 kingdoms :
+			</p>
+
+			<ul>
+				<li><a href="/continent/mountain_kingdom">The mountain kingdom</a></li>
+				<li><a href="/continent/desert_kingdom">The desert kingdom</a></li>
+				<li><a href="/continent/underwater_kingdom">The underwater kingdom</a></li>
+				<li><a href="/continent/underground_kingdom">The underground kingdom</a></li>
+				<li><a href="/continent/sky_kingdom">The sky kingdom</a></li>
+				<li><a href="/continent/sun_kingdom">The sun kingdom</a></li>
+				<li><a href="/continent/fire_kingdom">The fire kingdom</a></li>
+			</ul>
+
+			<p>
+				To those kingdoms is added <a href="/continent/peace_island">peace island</a>, a neutral ground.
+			</p>
+
+			<h2>The corrupted lands</h2>
+
+			<p>
+				The <a href="/continent/corrupted_lands">corrupted lands</a> are a big infected region by radiations and mutated creatures.
+				It is said that those lands are cursed and void of life, but in reality they are inhabited
+				by the rebellion, a group of people fighting against the kingdoms of the good lands.
+			</p>
+
+			<h2>The isolated lands</h2>
+
+			<p>
+				The isolated lands are at the opposite of the good lands, separated by the corrupted lands.
+				They can themselves be divided into 4 regions :
+			</p>
+
+			<ul>
+				<li><a href="/continent/cold_kingdom">The cold kingdom</a></li>
+				<li><a href="/continent/heaven_road">The Heaven road</a></li>
+				<li><a href="/continent/jungle">The jungle</a></li>
+				<li><a href="/continent/lost_island">Lost island</a></li>
+			</ul>
+
+			<h2>Other</h2>
+
+			<p>
+				Other places of interest in Edmayne include :
+			</p>
+
+			<ul>
+				<li><a href="/continent/deadly_waters">The deadly waters</a></li>
+				<li><a href="/continent/barriers">The Barriers</a></li>
+				<li><a href="/continent/holy_kingdom">The Holy kingdom</a></li>
+			</ul>
+
+		</div>
+	</div>
+
+</div>
